78acc018723c8fb74b0b973990025e97313bbd89
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-04-02 22:53:25 -0400

    Default Spring Profile

--- src/main/java/guru/springframework/sfg/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfg/services/I18nSpanishGreetingService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfg.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
